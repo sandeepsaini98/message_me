f677c5f667e7b305fe4c416752030ecf5114adb9
sandeepsaini98 <sandeepsaini85113@gmail.com>
2021-09-28 19:18:48 +0530

    Added helper methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Sandeep", password: "password")
+User.create(username: "Rajat", password: "password")
+User.create(username: "Machine", password: "password")
+User.create(username: "Ashutosh", password: "password")
+User.create(username: "Vivek", password: "password")
